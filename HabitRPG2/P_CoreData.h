cc3d912f2d0c5cc7dad3b8cc150ae17f6951510b
Joel Pridgen <joelpridgen@Joels-MacBook-Pro.local>
2017-03-27 17:38:51 -0400

    Added the protocol that is to be the base for CoreDataStackController

---
+++ HabitRPG2/P_CoreData.h
@@ -0,0 +1,37 @@
+//
+//  P_CoreData.h
+//  HabitRPG2
+//
+//  Created by Joel Pridgen on 3/27/17.
+//  Copyright © 2017 Joel Pridgen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DataInfo+CoreDataClass.h"
+#import "Settings+CoreDataClass.h"
+#import "OnlyOneEntities.h"
+#import "constants.h"
+@class OnlyOneEntities;
+
+@protocol P_CoreData <NSObject>
+@property (nonatomic,strong) NSManagedObjectContext *context;
+@property (nonatomic,assign) BOOL disableSave;
+@property (nonatomic,assign) BOOL disabledSaveResult;
+@property (nonatomic,assign) BOOL isTesting;
+@property (nonatomic,strong) OnlyOneEntities *userData;
+-(instancetype)initWithDBFileName: (NSString *) dbFileName;
+-(NSManagedObject *)constructEmptyEntity:(NSString *) entityType;
+-(NSFetchedResultsController *)getItemFetcher:(NSString *) entityName
+                                    predicate: (NSPredicate *) filter
+                                       sortBy:(NSArray *) sortAttrs;
+-(NSManagedObject *)getItem:(NSString *) entityName
+                  predicate: (NSPredicate *) filter
+                     sortBy:(NSArray<NSSortDescriptor *> *) sortAttrs;
+-(NSManagedObject *)getItemWithRequest:(NSFetchRequest *) fetchRequest
+                             predicate: (NSPredicate *)filter
+                                sortBy: (NSArray<NSSortDescriptor *> *)sortArray;
+-(BOOL)save;
+-(void)softDeleteModel:(NSManagedObject *)model;
+-(BOOL)deleteModelAndSave:(NSManagedObject *)model;
+-(void)deleteAllRecords;
+@end
